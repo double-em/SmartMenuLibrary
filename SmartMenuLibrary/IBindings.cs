6bf7386ff7a5eaf2f74d9aef49ab40add157ccd8
Zxited <mmgamesfull@gmail.com>
2018-10-04 14:13:52 +0200

    Added Library Items

--- SmartMenuLibrary/IBindings.cs
+++ SmartMenuLibrary/IBindings.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace SmartMenuLibrary
 {
-    public class Class1
+    public interface IBindings
     {
+        string Call(string menuID);
     }
 }
---
+++ TestVersion1/TestVersion1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartMenuLibrary;
+using System.IO;
+
+namespace SmartMenuTest
+{
+    [TestClass]
+    public class TestVersion1
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            SmartMenu m = new SmartMenu();
+            string path = "MenuSpec";
+            path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\MenuErrors.txt"));
+            m.LoadMenu(path);
+        }
+    }
+}
